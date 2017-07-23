31e7ec050cba16f2298ff8cc9ee807cfdd559760
Arko Gupta <arkogupta16@gmail.com>
2017-07-23 14:30:49 +0530

    Initial commit

---
+++ Iris_Classifier.py
@@ -0,0 +1,79 @@
+# Load libraries
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+
+
+#load dataset
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal_length', 'sepal_width', 'petal_length' ,'petal_width', 'class']
+dataset = pandas.read_csv(url,names=names)
+
+#get shape
+print(dataset.shape)
+
+
+#dataset.head(k) ->prints first k training examples
+ print(dataset.head(20))
+
+
+# #detailed description of dataset
+# print(dataset.describe())
+
+
+# class distribution
+ print(dataset.groupby('class').size())
+
+
+# histograms
+dataset.hist()
+plt.show()
+
+#scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+#check all algos
+models = []
+models.append(('LR',LogisticRegression()))
+models.append(('SVM',SVC()))
+
+
+#evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+
+# Make predictions on validation dataset
+lr = LogisticRegression()
+lr.fit(X_train,Y_train);
+predictions = lr.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
